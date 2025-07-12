8992e45d8e4b3cb6506d67d3ee79a45eabcc23f3
reddoctor <1659536949@qq.com>
2025-07-12 22:01:18 +0800

    Release: 版本号更新
    - versionCode: 1
    - versionName: 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,9 +12,8 @@ android {
         applicationId = "com.reddoctor.threadui"
         minSdk = 29
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.2"
-
+        versionCode = 1
+        versionName = "1.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
